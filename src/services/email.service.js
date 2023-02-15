4cc65c776079f903a0ccd3c66e2afd2c216feaf1
AhmedKhamis <akhamis574@gmail.com>
2023-02-15 19:16:02 +0200

    change in email

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -11,6 +11,7 @@ if (config.env !== 'test') {
     .catch(() => logger.warn('Unable to connect to email server. Make sure you have configured the SMTP options in .env'));
 }
 
+
 /**
  * Send an email
  * @param {string} to
